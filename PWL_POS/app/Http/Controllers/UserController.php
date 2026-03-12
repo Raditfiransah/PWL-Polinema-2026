7844c5cea6aee2d2465d5543260005902b8e35b6
raditfiransah <raditfiransah180@gmail.com>
2026-03-12 21:08:04 +0700

    hasil Praktikum-2.4: comparing firstOrNew and firstOrCreate

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,8 +10,12 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        dd($user);
+        $user = UserModel::firstOrNew([
+            'username' => 'manager23',
+            'nama' => 'Manager dua tiga',
+            'password' => Hash::make('password'),
+            'level_id' => 5,
+        ]);
         return view('user', ['data' => $user]);
     }
 }
